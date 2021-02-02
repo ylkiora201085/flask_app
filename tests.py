426c755f2898d136dd2fe83f61d368249ee63b0e
Ivanov Anton <ylkiora201085@mail.ru>
2021-02-02 12:27:42 +0300

    add znaki

--- tests.py
+++ tests.py
@@ -9,7 +9,7 @@ class FlaskAppTests(unittest.TestCase):
 
     def test_get(self):
         r = self.app.get('/')
-        self.assertEqual(r.data.decode('utf-8'), 'У меня получилось!')
+        self.assertEqual(r.data.decode('utf-8'), 'У меня получилось!!!')
 
     def test_post(self):
         r = self.app.post('/')
